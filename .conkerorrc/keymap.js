e696ec23dee331b4c3461806c2632760c07c4934
DK <beerandhot@gmail.com>
2013-05-31 15:12:45 +0400

    redefine simpler

--- .conkerorrc/keymap.js
+++ .conkerorrc/keymap.js
@@ -1,14 +1,13 @@
 //keybindings 
-ru = decodeURIComponent(escape('ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'));
-en = '`qwertyuiop[]asdfghjkl;\'zxcvbnm,.~QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>';
-
-function e2r (ch) {
-    let (idx = en.indexOf(ch)) {
-        return (idx == -1)? ch : ru[idx];
-    }
-}
-
 function tr (seq) {
+    ru = decodeURIComponent(escape('ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'));
+    en = '`qwertyuiop[]asdfghjkl;\'zxcvbnm,.~QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>';
+    function e2r (ch) {
+        let (idx = en.indexOf(ch)) {
+            return (idx == -1)? ch : ru[idx];
+        }
+    }
+
     var nseq = [];
     seq.forEach(
         function (tmp) {
@@ -24,49 +23,16 @@ function tr (seq) {
     return nseq;
 }
 
-// hacked version
-define_keywords("$fallthrough", "$repeat", "$browser_object");
-function define_key (kmap, seq, cmd) {
-    keywords(arguments);
-    var orig_seq = seq;
-    try {
-        var ref = get_caller_source_code_reference();
+// defadvice emulation
+define_key_internal = (function (fn) {
+    return function () {
+        fn.apply(fn, arguments);
+        arguments[2] = tr(arguments[2]);
+        fn.apply(fn, arguments);
+    };
+})(define_key_internal);
 
-        if (typeof seq == "string" && seq.length > 1)
-            seq = seq.split(" ");
 
-        if (!(typeof seq == "object") || !(seq instanceof Array))
-            seq = [seq];
-
-        // normalize the order of modifiers in key combos
-        seq = seq.map(
-            function (k) {
-                if (typeof(k) == "string")
-                    return format_key_combo(unformat_key_combo(k));
-                else
-                    return k;
-            });
-
-        var new_command = null;
-        var new_keymap = null;
-        if (typeof cmd == "string" || typeof cmd == "function")
-            new_command = cmd;
-        else if (cmd instanceof keymap)
-            new_keymap = cmd;
-        else if (cmd != null)
-            throw new Error("Invalid `cmd' argument: " + cmd);
-
-        define_key_internal(ref, kmap, seq, new_command, new_keymap,
-                            forward_keywords(arguments));
-        // repeat for translated version
-        define_key_internal(ref, kmap, tr(seq), new_command, new_keymap,
-                          forward_keywords(arguments));
-
-    } catch (e if (typeof e == "string")) {
-        dumpln("Warning: Error occurred while binding sequence: " + orig_seq);
-        dumpln(e);
-    }
-}
 // do not load default bindings before rc (will work from the second time)
 user_pref("conkeror.load.bindings/default/bindings", 0);
 // but load after
@@ -78,3 +44,7 @@ define_key(text_keymap, 'C-w', 'cmd_deleteWordBackward');
 define_key(text_keymap, 'C-h', 'cmd_deleteCharBackward');
 require("global-overlay-keymap.js");
 define_key_alias("C-m", "return");
+
+// hinting
+hints_auto_exit_delay=250;
+hints_ambiguous_auto_exit_delay=750;
