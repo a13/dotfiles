552938db7eefe9c3836bbbaecb6dcaa209ce981c
Dmytro <beerandhot@gmail.com>
2024-11-21 02:09:42 +0200

    improve path generator, fix quoting

--- envset.clj
+++ envset.clj
@@ -4,6 +4,22 @@
   (:require [clojure.edn :as edn]
             [clojure.string :as str]))
 
+(defn flatten-paths
+  "Convert a nested hashmap to [path] value one."
+  ([m]
+   (flatten-paths m []))
+  ([m path]
+   (reduce-kv
+    (fn [acc k v]
+      (let [new-key (if path
+                      (conj path k)
+                      k)]
+        (if (map? v)
+          (merge acc (flatten-paths v new-key))
+          (assoc acc new-key v))))
+    {}
+    m)))
+
 (defn- path->env-name [path]
   (->> path
        (map (comp #(str/replace % #"\-" "_") str/upper-case name))
@@ -13,29 +29,16 @@
   (cond
     (vector? value) (str/join ":" value) ; PATH
     (number? value) value
-    (string? value) (format "'%s'" value)
-    :else (format "'%s'" (str value))))
+    (string? value) (format "\"%s\"" value)
+    :else (format "'%s'/" (str value))))
 
 (defn- envmap->source [m]
   (->> m
-       (map (fn [[path  value]]
+       (map (fn [[path value]]
               (str (path->env-name path) "=" (pp-value value))))
        sort
        (str/join "\n")))
 
-;; based on https://andersmurphy.com/2019/11/30/clojure-flattening-key-paths.html
-(defn flatten-paths
-  "Convert a nested hashmap to path:value one."
-  ([m path]
-   (lazy-seq
-    (when-let [[[k v] & xs] (seq m)]
-      (let [rest-flatten (flatten-paths xs path)
-            path* (conj path k)]
-        (if (and (map? v) (seq v))
-          (into (flatten-paths v path*) rest-flatten)
-          (cons [path* v] rest-flatten))))))
-  ([m]
-   (into {} (flatten-paths m []))))
 
 (comment
   (->> (slurp "env.edn")
